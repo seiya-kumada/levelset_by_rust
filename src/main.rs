ee4d6eb4caa5176d3459cb3a9b5d7ca07dfec879
seiya_kumada <kumada@kumadakiyoshinarinoMacBook-Pro.local>
2023-01-10 10:48:15 +0900

    kappaがおかしい？

--- src/main.rs
+++ src/main.rs
@@ -3,160 +3,111 @@ pub mod core;
 pub mod interface;
 pub mod test;
 use crate::core::grid::Grid3d;
-use crate::core::indexer::Indexer3d;
 use crate::core::indexer::IndexerMethod;
-use crate::core::initial_front::InitialFront3d;
+use crate::core::indexer::{Indexer2d, Indexer3d};
+use crate::core::initial_front::{InitialFront2d, InitialFront3d};
 use crate::core::inside_estimator::{InsideEstimator3d, InsideEstimatorMethod};
-use crate::core::level_set_method::LevelSetMethod3d;
+use crate::core::level_set_method::{LevelSetMethod2d, LevelSetMethod3d};
 use crate::core::parameters::Parameters;
-use crate::core::point::Point3d;
-use crate::core::space_size::SpaceSize3d;
+use crate::core::point::{Point2d, Point3d};
+use crate::core::space_size::{SpaceSize2d, SpaceSize3d};
 use crate::core::status::Status;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
-fn make_input_gray(size: &SpaceSize3d, front: &InitialFront3d) -> Rc<RefCell<Vec<u8>>> {
+fn make_input_gray_2d(size: &SpaceSize2d, front: &InitialFront2d) -> Rc<RefCell<Vec<u8>>> {
     let mut gray = vec![1u8; size.total as usize];
     let left = front.vertices[0].x;
     let top = front.vertices[0].y;
-    let front_ = front.vertices[0].z;
     let right = front.vertices[1].x;
     let bottom = front.vertices[1].y;
-    let back = front.vertices[1].z;
 
-    let indexer = Indexer3d::new(&size);
+    let indexer = Indexer2d::new(&size);
 
-    for j in top..bottom {
-        for i in left..right {
-            let p = Point3d::<i32>::new(i, j, front_);
-            let q = Point3d::<i32>::new(i, j, back);
-            let p_index = indexer.get(&p) as usize;
-            let q_index = indexer.get(&q) as usize;
-            gray[p_index] = 0u8;
-            gray[q_index] = 0u8;
-        }
+    for i in left..(right + 1) {
+        let p = Point2d::<i32>::new(i, top);
+        let q = Point2d::<i32>::new(i, bottom);
+        let p_index = indexer.get(&p) as usize;
+        let q_index = indexer.get(&q) as usize;
+        gray[p_index] = 0u8;
+        gray[q_index] = 0u8;
     }
 
-    for j in top..bottom {
-        for k in front_..back {
-            let p = Point3d::<i32>::new(left, j, k);
-            let q = Point3d::<i32>::new(right, j, k);
-            let p_index = indexer.get(&p) as usize;
-            let q_index = indexer.get(&q) as usize;
-            gray[p_index] = 0u8;
-            gray[q_index] = 0u8;
-        }
+    for j in top..(bottom + 1) {
+        let p = Point2d::<i32>::new(left, j);
+        let q = Point2d::<i32>::new(right, j);
+        let p_index = indexer.get(&p) as usize;
+        let q_index = indexer.get(&q) as usize;
+        gray[p_index] = 0u8;
+        gray[q_index] = 0u8;
     }
 
-    for i in left..right {
-        for k in front_..back {
-            let p = Point3d::<i32>::new(i, top, k);
-            let q = Point3d::<i32>::new(i, bottom, k);
-            let p_index = indexer.get(&p) as usize;
-            let q_index = indexer.get(&q) as usize;
-            gray[p_index] = 0u8;
-            gray[q_index] = 0u8;
-        }
-    }
     Rc::new(RefCell::new(gray))
 }
 
 pub fn main() {
     let mut params = Parameters::new();
-    params.wband = 3;
+    params.wband = 1;
     params.constant_speed = 1.0;
     params.gain = 2.0;
-    params.wreset = 1;
 
-    let mut initial_front = InitialFront3d::new();
+    let mut initial_front = InitialFront2d::new();
     let left = 2;
     let top = 3;
     let right = 8;
     let bottom = 7;
-    let front = 3;
-    let back = 7;
 
-    initial_front.vertices[0] = Point3d::<i32>::new(left, top, front);
-    initial_front.vertices[1] = Point3d::<i32>::new(right, bottom, back);
+    initial_front.vertices[0] = Point2d::<i32>::new(left, top);
+    initial_front.vertices[1] = Point2d::<i32>::new(right, bottom);
 
-    let size = Rc::new(SpaceSize3d::new(11, 11, 11));
-    let gray = make_input_gray(&size, &initial_front);
-    let mut lsm = LevelSetMethod3d::new(params, Rc::clone(&size), Rc::clone(&gray));
-    lsm.initialize_distance_map();
+    let size = Rc::new(SpaceSize2d::new(11, 11));
+    let gray = make_input_gray_2d(&size, &initial_front);
+
+    let mut lsm = LevelSetMethod2d::new(params, Rc::clone(&size), Rc::clone(&gray));
     lsm.initialize_along_front(&initial_front);
     lsm.initialize_over_all(&initial_front);
+
     lsm.calculate_speed_factors();
+    lsm.initialize_narrow_band();
 
-    let resets = true;
+    let fs = lsm.set_speed_on_front();
+    assert!(fs != 0.0);
 
-    lsm.clear_speed_within_narrow_band(resets);
-    lsm.set_speed_on_front();
+    let width = size.width;
+    let height = size.height;
 
-    lsm.copy_nearest_speed_to_narrow_band(resets);
+    let speed = lsm.get_speed();
 
-    let mut status_map: HashMap<usize, Status> = HashMap::new();
-    status_map.insert(0, Status::Farway);
-    status_map.insert(1, Status::Band);
-    status_map.insert(2, Status::ResetBand);
-    status_map.insert(3, Status::Front);
-
-    let status_answers: Vec<usize> = vec![
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1,
-        1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2,
-        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1,
-        3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1,
-        1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 0, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1,
-        1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3,
-        1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 0, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1,
-        1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3,
-        3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 0, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1,
-        1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3,
-        3, 3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 0, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3,
-        1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3, 3,
-        3, 3, 3, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 0, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1,
-        1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1,
-        1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2,
-        2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2,
-        2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    ];
-    let statuses = lsm.get_statuses();
-    for (i, status) in statuses.borrow().iter().enumerate() {
-        let a = status_answers[i];
-        let b = status_map[&a];
-        let c = *status;
-        if b != c {
-            println!(">> {}", i)
+    for j in 0..height {
+        let wj = width * j;
+        for i in 0..width {
+            let index = (wj + i) as usize;
+            if (i == left && j == top) {
+                assert!(0.0 != speed.borrow()[index]);
+            } else if (left < i && i <= right && j == top) {
+                assert!(0.0 != speed.borrow()[index]);
+            } else if (i == right && top <= j && j < bottom) {
+                assert!(0.0 != speed.borrow()[index]);
+            } else if (i == right && j == bottom) {
+                //assert!(0.0 != speed.borrow()[index]);
+                //if 0.0 == speed.borrow()[index] {
+                //    println!("D: {},{}", index, speed.borrow()[index]);
+                //}
+            } else if (left <= i && i < right && j == top) {
+                assert!(0.0 != speed.borrow()[index]);
+            } else if (i == left && top < j && top <= bottom) {
+                //assert!(0.0 != speed.borrow()[index]);
+                //if 0.0 == speed.borrow()[index] {
+                //    println!("E: {},{}", index, speed.borrow()[index]);
+                //}
+            } else {
+                //assert!(0.0 == speed.borrow()[index]);
+                //if 0.0 != speed.borrow()[index] {
+                //    println!("F: {},{}", index, speed.borrow()[index]);
+                //}
+            }
         }
-        //assert_eq!(b, c);
     }
 }
 //不変参照(&,borrow)と可変参照(&mut,borrow_mut)
--- src/test/test_level_set_method.rs
+++ src/test/test_level_set_method.rs
@@ -248,6 +248,36 @@ mod tests {
         }
     }
 
+    fn make_input_gray_2d(size: &SpaceSize2d, front: &InitialFront2d) -> Rc<RefCell<Vec<u8>>> {
+        let mut gray = vec![1u8; size.total as usize];
+        let left = front.vertices[0].x;
+        let top = front.vertices[0].y;
+        let right = front.vertices[1].x;
+        let bottom = front.vertices[1].y;
+
+        let indexer = Indexer2d::new(&size);
+
+        for i in left..(right + 1) {
+            let p = Point2d::<i32>::new(i, top);
+            let q = Point2d::<i32>::new(i, bottom);
+            let p_index = indexer.get(&p) as usize;
+            let q_index = indexer.get(&q) as usize;
+            gray[p_index] = 0u8;
+            gray[q_index] = 0u8;
+        }
+
+        for j in top..(bottom + 1) {
+            let p = Point2d::<i32>::new(left, j);
+            let q = Point2d::<i32>::new(right, j);
+            let p_index = indexer.get(&p) as usize;
+            let q_index = indexer.get(&q) as usize;
+            gray[p_index] = 0u8;
+            gray[q_index] = 0u8;
+        }
+
+        Rc::new(RefCell::new(gray))
+    }
+
     fn make_input_gray(size: &SpaceSize3d, front: &InitialFront3d) -> Rc<RefCell<Vec<u8>>> {
         let mut gray = vec![1u8; size.total as usize];
         let left = front.vertices[0].x;
@@ -292,6 +322,63 @@ mod tests {
             }
         }
         Rc::new(RefCell::new(gray))
+    }
+
+    #[test]
+    fn set_speed_on_front_2d() {
+        let mut params = Parameters::new();
+        params.wband = 1;
+        params.constant_speed = 1.0;
+        params.gain = 2.0;
+
+        let mut initial_front = InitialFront2d::new();
+        let left = 2;
+        let top = 3;
+        let right = 8;
+        let bottom = 7;
+
+        initial_front.vertices[0] = Point2d::<i32>::new(left, top);
+        initial_front.vertices[1] = Point2d::<i32>::new(right, bottom);
+
+        let size = Rc::new(SpaceSize2d::new(11, 11));
+        let gray = make_input_gray_2d(&size, &initial_front);
+
+        let mut lsm = LevelSetMethod2d::new(params, Rc::clone(&size), Rc::clone(&gray));
+        lsm.initialize_along_front(&initial_front);
+        lsm.initialize_over_all(&initial_front);
+
+        lsm.calculate_speed_factors();
+        lsm.initialize_narrow_band();
+
+        let fs = lsm.set_speed_on_front();
+        assert!(fs != 0.0);
+
+        let width = size.width;
+        let height = size.height;
+
+        let speed = lsm.get_speed();
+
+        for j in 0..height {
+            let wj = width * j;
+            for i in 0..width {
+                let index = (wj + i) as usize;
+                if (i == left && j == top) {
+                    assert!(0.0 != speed.borrow()[index]);
+                } else if (left < i && i <= right && j == top) {
+                    assert!(0.0 != speed.borrow()[index]);
+                } else if (i == right && top <= j && j < bottom) {
+                    assert!(0.0 != speed.borrow()[index]);
+                } else if (i == right && j == bottom) {
+                    assert!(0.0 != speed.borrow()[index]);
+                } else if (left <= i && i < right && j == top) {
+                    assert!(0.0 != speed.borrow()[index]);
+                } else if (i == left && top < j && top <= bottom) {
+                    assert!(0.0 != speed.borrow()[index]);
+                } else {
+                    assert!(0.0 == speed.borrow()[index]);
+                }
+            }
+        }
     }
 
     #[test]
